040fb9443ed0e50072d070d66a881ed69d89b00b
semih <semih@simplisales.com>
2022-12-28 05:32:02 +0300

    register form added

---
+++ src/components/molecules/RegisterForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="register-form">
+     <AtomInput placeholder="Adınız Soyadınız"></AtomInput>
+     <AtomInput placeholder="E-Posta Adresiniz"></AtomInput>
+     <AtomInput placeholder="Parolanız"></AtomInput>
+     <AtomButton button-txt="Giriş Yap" button-color="#8CA632"></AtomButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AtomButton from "../atoms/AtomButton.vue";
+import AtomInput from "../atoms/AtomInput.vue";
+
+</script>
+
+<style lang="scss" scoped>
+.register-form {
+    display: flex;
+    flex-direction: column;
+}
+</style>
